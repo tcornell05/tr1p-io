52d0e82d8c74e6063f32a720a9157c19312828fc
tcornell05 <tcornell05@gmail.com>
2024-08-09 16:12:35 -0400

    New article

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: process.env.ASSET_PREFIX || '',
-    images: {
-        domains: ['aceternity.com'], // Add 'aceternity.com' to the domains array
-    },
+  assetPrefix: process.env.ASSET_PREFIX || '',
+  images: {
+    domains: ['aceternity.com'], // Add 'aceternity.com' to the domains array
+  },
+  optimizeFonts: false,
 }
 
 module.exports = nextConfig
